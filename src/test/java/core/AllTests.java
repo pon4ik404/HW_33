3223a80425d3f37a17a41611461a559a3d984739
Maria Nasonoff <marianasonoff@gmail.com>
2020-04-08 19:18:59 -0700

    initial commit

---
+++ src/test/java/core/AllTests.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import core.PrimeNumberCheckerTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({PrimeNumberCheckerTest.class})
+public class AllTests {
+
+}
